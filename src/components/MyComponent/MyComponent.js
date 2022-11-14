595a921f8c799319a76ec0829a930c5dadf6af31
rassellek <rassellek@bk.ru>
2022-11-14 22:48:47 +0500

    edit react

---
+++ src/components/MyComponent/MyComponent.js
@@ -0,0 +1,17 @@
+import './MyComponent.css';
+
+function MyComponent() {
+    return (
+        <div>
+            <span>Hello,</span>
+            <span>functional</span>
+            <span>component</span>
+        </div>
+    )
+}
+
+export function MyComponent2() {
+
+}
+
+export default MyComponent;
